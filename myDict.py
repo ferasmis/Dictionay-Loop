7695c2c96c4b4806fd574b428f34d70c36fa1e63
ferasmis <60306379+ferasmis@users.noreply.github.com>
2020-10-26 20:33:08 +0300

    Add files via upload

---
+++ myDict.py
@@ -0,0 +1,7 @@
+## Author: Feras
+## Description: A for-loop to display both keys and values of the given mydict variable
+## Variable: mydict = {'one': 'value1', 'two': 'value2'}
+
+mydict = {'one': 'value1', 'two': 'value2'}
+for i in mydict:
+    print(i,mydict[i])
